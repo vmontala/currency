53ff13d92fd1d3805fc7921c917fb1c2a78e13ad
Victor Montala <victormontala@gmail.com>
2025-03-25 17:11:50 +0100

    Configured layout

---
+++ src/layout/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="main">
+    <slot />
+  </main>
+</template>
+
+<style scoped>
+.main {
+  max-width: 80rem;
+  width: 100%;
+  padding: var(--s-xl) var(--s-md) var(--s-md);
+  display: flex;
+  flex-direction: column;
+  gap: var(--s-xl);
+}
+</style>
