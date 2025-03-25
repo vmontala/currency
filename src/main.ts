4d6fc909e4b8236139aee0b096276f8e03761912
Victor Montala <victormontala@gmail.com>
2025-03-25 17:10:21 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import 'reset-css'
+import '@fontsource/source-sans-pro';
+
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#root')
